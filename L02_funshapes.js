65b9ff6f714d1f7840cce7a4f618335c69860f84
Computhink-CS124 <cs124@computhink.com.sg>
2025-07-19 06:49:41 +0000

    Jul 19, 2025, 6:49 AM

--- L02_funshapes.js
+++ L02_funshapes.js
@@ -6,7 +6,7 @@ createCanvas(600,400)
 background(111);
 }
 function draw() {
-  eclips
+  eclipse(100,100,80)
   // Challenge 1: An eye using ellipse() function
 
   // Challenge 2: A face using circle() function
