2b2626b78f07787586fbc1d9ebc6fc0ccb947b6f
Computhink-CS124 <cs124@computhink.com.sg>
2025-07-19 07:11:35 +0000

    Jul 19, 2025, 7:11 AM

--- L02_funshapes.js
+++ L02_funshapes.js
@@ -14,7 +14,7 @@ function draw() {
   circle(400,100,100)
   circle(250,150,50)
   circle(350,150,50)
-  ellipse(300)
+  ellipse(300,200,100,50)
   // Challenge 3: A house using rect() function
 
   // Challenge 4: A rocketship using triangle() function
