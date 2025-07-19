179cf18219706d23a8e39463915933b0b7e8e0ce
Computhink-CS124 <cs124@computhink.com.sg>
2025-07-19 07:29:02 +0000

    Jul 19, 2025, 7:29 AM

--- L02_funshapes.js
+++ L02_funshapes.js
@@ -17,7 +17,7 @@ triangle(30,75,58,20,86,75)
   // Challenge 4: A rocketship using triangle() function
 
   // Challenge 5: A square face using quad() function
-quad()
+quad(20,20,80,20,80,80,20,80)
   // Challenge 6: Add some colours to the drawings in the above challenges
 
   // Challenge 7: Create a funny Pokemon
