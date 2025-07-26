e8ef6ed7d571195e6d155cc780c36c5114b8855f
Computhink-CS124 <cs124@computhink.com.sg>
2025-07-26 07:03:40 +0000

    Jul 26, 2025, 7:03 AM

--- L03_patterns.js
+++ L03_patterns.js
@@ -10,8 +10,8 @@ function draw() {
   let spacing = 50;
 for (let i = 0; i < 5; i++){
   circle(spacing + i * spacing,100,40);
-  fill(colourvalue);
-  colourvalue +=1;
+  fill(colourValue);
+  colourValue +=1;
 }
   // Recap 1: Repeating Circles}
 
