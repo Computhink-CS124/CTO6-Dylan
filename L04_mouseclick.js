823e8e4b5ca9beb8cafc156d403c2715cf8c5e9c
Computhink-CS124 <cs124@computhink.com.sg>
2025-08-16 07:09:34 +0000

    Aug 16, 2025, 7:09 AM

--- L04_mouseclick.js
+++ L04_mouseclick.js
@@ -1,20 +1,20 @@
 // write your codes here
 let shapeColor ="blue";
-let circleSize = 20;
+let circleSize = 20
 function setup(){
 createCanvas(400,400)
 background(200);
 }
 
-function draw(){
-fill(shapeColor);
-ellipse(200,200,80,80)
-ellipse(mousX,mouseY,circleSize,circleSize)
-}
+// function draw(){
+// fill(shapeColor);
+// ellipse(200,200,80,80)
+// ellipse(mousX,mouseY,circleSize,circleSize)
+// }
 
 function mousePressed(){
 shapeColor = color(random(255), random(255),random(255))
-circlesize += 20;
+circlesize += 20
 }
 
 function mouseReleased(){
