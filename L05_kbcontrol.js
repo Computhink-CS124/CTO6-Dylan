43af409a11da2f21468decd72db3fbb7f48b3b3a
Computhink-CS124 <cs124@computhink.com.sg>
2025-08-23 06:37:47 +0000

    Aug 23, 2025, 6:37 AM

--- L05_kbcontrol.js
+++ L05_kbcontrol.js
@@ -1,17 +1,22 @@
-let shapeColor = " "
-let shapeSize = 5
+
+let shapeSize = 5;
 
 
 function setup(){
-createCanvas(600,400)
-background(200);
+    createCanvas(600,400)
+    background(220);
 }
+function draw() {
 
+
+}
 function mouseDragged () {
     shapeSize += 0.5;
     ellipse(mouseX,mouseY,shapeSize,shapeSize)
-    FileList(shapeColor)
+
 }
-function mouseReleased () {
-    shapeColor = (random(255),random(255),random(255))
+function mousePressed () {
+    fill(random(255),random(255),random(255));
+    shapeSize = 5;
+
 }
