4e61ced7fc53e715374fa3a8ff1565de9edc178f
Computhink-CS124 <cs124@computhink.com.sg>
2025-08-23 07:27:07 +0000

    Aug 23, 2025, 7:27 AM

--- L05_kbcontrol.js
+++ L05_kbcontrol.js
@@ -12,12 +12,10 @@ function draw() {
 
 }
 function keyDragged () {
-    shapeSize += 0.3;
-    ellipse(mouseX,mouseY,shapeSize,shapeSize)
+    
+    
 
 }
-function mousePressed () {
-    fill(random(255),random(255),random(255));
-    shapeSize = 5;
+function keyPressed () {
 
 }
